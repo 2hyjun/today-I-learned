138c379ce59e4649d1d08e08ee97509f80332ece
2hyjun <biper94@gmail.com>
2019-04-01 16:06:35 +0900

    Initial Configuration

---
+++ Spring/spring-boot-journal/src/main/kotlin/com/example/springbootjournal/SpringBootJournalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootjournal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootJournalApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootJournalApplication>(*args)
+}
